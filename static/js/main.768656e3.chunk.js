4d95796ac6cd9f9cf47f4d754bf87da4d3d1659f
Ivan Kuchvara <ivan.kuchvara@gmail.com>
2021-04-17 21:17:55 +0300

    Updates

---
+++ static/js/main.768656e3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var r=c(1),n=c.n(r),o=c(3),i=c.n(o),a=c(0);var d=function(){return Object(a.jsx)("div",{children:" Hello world"})};i.a.render(Object(a.jsx)(n.a.StrictMode,{children:Object(a.jsx)(d,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.768656e3.chunk.js.map
